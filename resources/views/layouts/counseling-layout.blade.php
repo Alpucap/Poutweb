e50d8a5ca6f416cbc1b523672bee71b1a085790d
unknown <alpucaps@gmail.com>
2024-06-20 11:21:41 +0700

    Adding changes on counseling

--- resources/views/layouts/counseling-layout.blade.php
+++ resources/views/layouts/counseling-layout.blade.php
@@ -6,6 +6,8 @@
     <script src="{{ asset('js/counseling-app.js') }}"></script>
     <link rel="icon" href="{{ asset('img\POUT.png') }}" type="image/x-icon"/>
     <link rel="shortcut icon" href="{{ asset('img\POUT.png') }}" type="image/x-icon"/>
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </head>
 <body>
     <div class="container">
